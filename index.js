9e85a416811376973d04cea7452764f0e9815747
Jerry Brown <brownjer3@gmail.com>
2018-02-26 20:16:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,9 @@ function nowServing(deliLine) {
 
 function currentLine(line) {
   var i = 0;
-  for (i = 0; i < 0; i++){ 
     if (line.length === 0) {
       return "The line is currently empty.";
-  }   else {
+    } else {
       return "The line is currently: 1. " + line[i];
     }
   }
-}
